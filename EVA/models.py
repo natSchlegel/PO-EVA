d0cce9a8bee4ce8f2e5e42fe05245d6c7528a6bd
natascha <nataschaschlegel27@gmail.com>
2022-08-20 22:38:07 -0300

    Postgres config

--- EVA/models.py
+++ EVA/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 class Ambiente(models.Model):
     nome = models.CharField(max_length=20, help_text='Entre o nome do ambiente')
-    porta = models.ForeignKey('Porta', on_delete=models.DO_NOTHING)
     inicio = models.BooleanField()
     metrosQuadrados = models.FloatField(help_text='Metros quadrados do ambiente')
+    ambientePosterior = models.ForeignKey('Ambiente', on_delete=models.DO_NOTHING)
+    porta = models.ForeignKey('Porta', on_delete=models.DO_NOTHING)
 
 class Porta(models.Model):
     nome = models.CharField(max_length=20, help_text='Identificador da porta')
     largura = models.FloatField(help_text='Tamanho da porta em metros')
     escada = models.BooleanField(help_text='Marque se tiver escada')
-    ambienteAnterior = models.ForeignKey('Ambiente', on_delete=models.DO_NOTHING)
-    ambientePosterior = models.ForeignKey('Ambiente', on_delete=models.DO_NOTHING)
-
 
